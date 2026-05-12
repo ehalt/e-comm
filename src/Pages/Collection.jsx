6d68fc8194586e984df22e4da6d91913c4739ef4
ehalt <torikussadikk@gmail.com>
2026-05-12 16:51:05 +0600

    updating collection comp

--- src/Pages/Collection.jsx
+++ src/Pages/Collection.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from './../context/ShopContext';
 
 const Collection = () => {
+
+  const { products } = useContext(ShopContext);
+
   return (
-    <div>
-      <h2>this is collection page</h2>
+    <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
+      {/* Filter options */}
+      <div className='min-w-60'>
+        <p className="my-2 text-xl flex items-center cursor-pointer gap-2">FILTERS</p>
+      </div>
     </div>
   )
 }
