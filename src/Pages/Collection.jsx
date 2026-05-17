2d4ea8f98710d29ca6942410082ba029670718a1
ehalt <torikussadikk@gmail.com>
2026-05-18 01:09:42 +0600

    adding items on collection page

--- src/Pages/Collection.jsx
+++ src/Pages/Collection.jsx
@@ -1,16 +1,91 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from './../context/ShopContext';
+import { assets } from '../assets/admin/assets';
+import Title from '../Components/Title';
+import ProductItem from './../Components/ProductItem';
+
 
 const Collection = () => {
 
   const { products } = useContext(ShopContext);
+  const [showFilter, setShowFilter] = useState(false);
+  const [filterProducts, setFilterProducts] = useState([]);
+  const [category, setCategory] = useState([]);
+  const [subCategory, setSubCategory] = useState([]);
+
+
+  const toggleCategory = (e) => {
+    if (category.includes(e.target.value)) {
+      setCategory(prev => prev.filter(item => item !== e.target.value));
+    }
+    else {
+      setCategory(prev => [...prev, e.target.value]);
+    }
+  }
+
+
+  useEffect(() => {
+    setFilterProducts(products);
+  },[])
 
   return (
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
       {/* Filter options */}
       <div className='min-w-60'>
-        <p className="my-2 text-xl flex items-center cursor-pointer gap-2">FILTERS</p>
+        <p onClick={() => setShowFilter(!showFilter)} className="my-2 text-xl flex items-center cursor-pointer gap-2">FILTERS
+          <img className={`h-3 sm:hidden ${showFilter ? 'rotate-90' : ''}`} src={assets.dropdown_icon} alt="" />
+        </p>
+        {/* Category filter */}
+        <div className={`border border-gray-300 pl-5 py-3 mt-6 ${showFilter ? '' :  'hidden'} sm:block`}>
+          <p className='mb-3 text-sm font-medium'>CATEGORIES</p>
+          <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
+            <p className="flex gap-2">
+              <input type="checkbox" value={'Men'} onChange={toggleCategory} className="w-3" />Men
+            </p>
+            <p className='flex gap-2'>
+              <input type="checkbox" value={'Women'} onChange={toggleCategory} className="w-3" />Women
+            </p>
+              <p className='flex gap-2'>
+               <input type="checkbox" value={'Kids'} onChange={toggleCategory} className="w-3" />Kids
+              </p>
+          </div>
+        </div>
+        {/* Sub category */}
+        <div className={`border border-gray-300 pl-5 py-3 my-5 ${showFilter ? '' :  'hidden'} sm:block`}>
+          <p className='mb-3 text-sm font-medium'>TYPE</p>
+          <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
+            <p className="flex gap-2">
+              <input type="checkbox" value={'Men'} className="w-3" />Topwear
+            </p>
+            <p className='flex gap-2'>
+              <input type="checkbox" value={'Women'} className="w-3" />Bottomwear
+            </p>
+              <p className='flex gap-2'>
+               <input type="checkbox" value={'Kids'} className="w-3" />Winterwear
+              </p>
+          </div>
+        </div>
       </div>
+        {/* Right side */}
+        <div className="flex-1">
+          <div className="flex justify-between text-base sm:text-2xl mb-4">
+            <Title text1={'ALL'} text2={'COLLECTIONS'} />
+            {/* Product Sort */}
+            <select className='border-2 border-gray-300 text-sm px-2'>
+              <option value="relavent">Sort by: Relavent</option>
+              <option value="low-high">Sort by: Low to High</option>
+              <option value="hight-low">Sort by: High to low</option>
+            </select>
+          </div>
+          {/* Map Products */}
+          <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-6'>
+            {
+              filterProducts.map((item, index) => (
+                <ProductItem key={index} name={item.name} id={item._id} price={item.price} image={item.image} />
+              ))
+            }
+          </div>
+        </div>
     </div>
   )
 }
