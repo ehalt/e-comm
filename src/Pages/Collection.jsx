4aa29b4437bc4fde498bee82fe2b213b69ecfd6d
ehalt <torikussadikk@gmail.com>
2026-05-18 11:35:45 +0600

    toggle sub category updated

--- src/Pages/Collection.jsx
+++ src/Pages/Collection.jsx
@@ -23,10 +23,33 @@ const Collection = () => {
     }
   }
 
+  const toggleSubCategory = (e) => {
+    if (subCategory.includes(e.target.value)) {
+      setSubCategory(prev => prev.filter(item => item !== e.target.value));
+    }
+    else {
+      setSubCategory(prev => [...prev, e.target.value]);
+    }
+  }
+
+  const applyFilter = () => {
+    let productCopy = products.slice();
+
+    if (category.length > 0) {
+      productCopy = productCopy.filter(item => category.includes(item.category));
+    }
+    if (subCategory.length > 0) {
+      productCopy = productCopy.filter(item => subCategory.includes(item.subCategory))
+    }
+
+    setFilterProducts(productCopy);
+  }
+
+
 
   useEffect(() => {
-    setFilterProducts(products);
-  },[])
+    applyFilter();
+  }, [category, subCategory, products]);
 
   return (
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
@@ -55,13 +78,13 @@ const Collection = () => {
           <p className='mb-3 text-sm font-medium'>TYPE</p>
           <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
             <p className="flex gap-2">
-              <input type="checkbox" value={'Men'} className="w-3" />Topwear
+              <input type="checkbox" value={'Topwear'} className="w-3" onChange={toggleSubCategory} />Topwear
             </p>
             <p className='flex gap-2'>
-              <input type="checkbox" value={'Women'} className="w-3" />Bottomwear
+              <input type="checkbox" value={'Bottomwear'} className="w-3" onChange={toggleSubCategory} />Bottomwear
             </p>
               <p className='flex gap-2'>
-               <input type="checkbox" value={'Kids'} className="w-3" />Winterwear
+               <input type="checkbox" value={'Winterwear'} className="w-3" onChange={toggleSubCategory} />Winterwear
               </p>
           </div>
         </div>
